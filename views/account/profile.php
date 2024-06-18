a72c4e78e277654cafae4af1ddb2b05b9e559f68
priyanka <1984royalrajput@gmail.com>
2024-06-18 08:51:26 +0530

    css fixed

--- views/account/profile.php
+++ views/account/profile.php
@@ -15,7 +15,7 @@
 <body>
 
 
-    <div class="m-2 text-center">
+    <div class=" m-2 text-center">
         <div class="position-relative">
             <img src="../images/Nature.png" class="w-100 h-50" alt="">
             <div class="position-absolute-top w-100 text center">
@@ -23,12 +23,15 @@
                 <p>Kaduna, Nigeria</p>
             </div>
         </div>
-        <div class="position-absolute-profile-div text-left w-25 h-75 ">
+        <div class="position-absolute-profile-div text-left w-25 h-fix shadow ">
             <div>
-                <img src="../images/profile-pic.webp" alt="" class="position-relative w-100 h-75">
-                <h2 class="position-absolute-profilepic">Follow Crid</h2>
+                <img src="../images/profile-pic.jpeg" alt="" class="position-relative w-100">
+                <div class="position-absolute-profilepic d-flex justify-contant-center overflow-auto">
+                    <h5 class="p-lr">Follow Crid</h5>
+                    <p class="icon"><i class="fa-solid fa-ellipsis" style="color: #00ffff;"></i></p>
+                </div>
             </div>
-            <div class="">
+            <div class="m-3">
                 <h4>About</h4>
                 <p class="border-bottom p-1">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum quas animi quisquam ut quam reprehenderit, labore cum ratione dolores nemo illo saepe perferendis! Sunt illo voluptas excepturi nostrum! Quam, explicabo?</p>
 
@@ -56,52 +59,52 @@
         </div>
 
         <div class="p-5">
-        <div class=" text-center">
-            <div class="d-flex justify-content-end ">
-                <div class="w-75">
-                    <h2>Edit Profile</h2>
-                    <p><b>Update Profile by entring the information below</b></p>
-                    <form class="mt-5" action="../../controller/updateprofilecontroller.php" method="POST">
-                        <div class="m-3">
-                            <div class="col mb-3 ">
-                                <div class="d-flex justify-content-around">
-                                    <input type="text" name="name" value="<?php echo $user['firstname'];?>" class="form-control w-75" placeholder="First name" aria-label="First name">
+            <div class=" text-center">
+                <div class="d-flex justify-content-end ">
+                    <div class="w-75">
+                        <h2>Edit Profile</h2>
+                        <p><b>Update Profile by entring the information below</b></p>
+                        <form class="mt-5" action="../../controller/updateprofilecontroller.php" method="POST">
+                            <div class="m-3">
+                                <div class="col mb-3 ">
+                                    <div class="d-flex justify-content-around">
+                                        <input type="text" name="name" value="<?php echo $user['firstname']; ?>" class="form-control w-75" placeholder="First name" aria-label="First name">
+                                    </div>
+                                    <?php
+                                    if (isset($errors['nameErr'])) { ?>
+                                        <p class="text-danger"><?php echo $errors['nameErr'] ?></p>
+                                    <?php  } ?>
                                 </div>
-                                <?php
-                                if (isset($errors['nameErr'])) { ?>
-                                    <p class="text-danger"><?php echo $errors['nameErr'] ?></p>
-                                <?php  } ?>
-                            </div>
-                            <div class="col mb-3">
-                                <div class="d-flex justify-content-around">
-                                    <input type="text" name="lastname" class="form-control w-75" placeholder="Last name" aria-label="Last name">
+                                <div class="col mb-3">
+                                    <div class="d-flex justify-content-around">
+                                        <input type="text" name="lastname" class="form-control w-75" placeholder="Last name" aria-label="Last name">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col mb-3">
-                                <div class="d-flex justify-content-around">
-                                    <input type="text" name="email" value="<?php echo $user['email'];?>" class="form-control w-75" placeholder="Email Id" aria-label="Email Id">
+                                <div class="col mb-3">
+                                    <div class="d-flex justify-content-around">
+                                        <input type="text" name="email" value="<?php echo $user['email']; ?>" class="form-control w-75" placeholder="Email Id" aria-label="Email Id">
+                                    </div>
+                                    <?php
+                                    if (isset($errors['emailErr'])) { ?>
+                                        <p class="text-danger"><?php echo $errors['emailErr'] ?></p>
+                                    <?php  } ?>
                                 </div>
-                                <?php
-                                if (isset($errors['emailErr'])) { ?>
-                                    <p class="text-danger"><?php echo $errors['emailErr'] ?></p>
-                                <?php  } ?>
-                            </div>
-                            <div class="col mb-3">
-                                <div class="d-flex justify-content-around">
-                                    <input type="password" name="password" value="<?php echo $user['password'];?>" class="form-control w-75" placeholder="Password" aria-label="Password">
+                                <div class="col mb-3">
+                                    <div class="d-flex justify-content-around">
+                                        <input type="password" name="password" value="<?php echo $user['password']; ?>" class="form-control w-75" placeholder="Password" aria-label="Password">
+                                    </div>
+                                    <?php
+                                    if (isset($errors['passwordErr'])) { ?>
+                                        <p class="text-danger"><?php echo $errors['passwordErr'] ?></p>
+                                    <?php  } ?>
                                 </div>
-                                <?php
-                                if (isset($errors['passwordErr'])) { ?>
-                                    <p class="text-danger"><?php echo $errors['passwordErr'] ?></p>
-                                <?php  } ?>
+                                <button type="submit" class="m-3 ps-5 pe-5 btn  btn-primary">Update</button>
                             </div>
-                            <button type="submit" class="m-3 ps-5 pe-5 btn  btn-primary">Update</button>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     </div>
 
 </body>
